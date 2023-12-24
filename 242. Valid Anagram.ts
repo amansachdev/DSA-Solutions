ba296c70da298b83c5daebf8689efd0193f88bca
aman.s4 <aman.s@kinaracapital.com>
2023-12-24 17:15:55 +0530

    242. Valid Anagram

---
+++ 242. Valid Anagram.ts
@@ -0,0 +1,5 @@
+function isAnagram(s: string, t: string): boolean {
+    const sortedS = s.split('').sort().join('');
+    const sortedT = t.split('').sort().join('');
+    return sortedS === sortedT ? true : false;
+};
