277f05d7cc836155afea5a648c61648abed1c692
aman.s4 <aman.s@kinaracapital.com>
2023-12-26 09:37:02 +0530

    Fixed

--- 20. Valid Parentheses.ts
+++ 20. Valid Parentheses.ts
@@ -1,3 +1,19 @@
 function isValid(s: string): boolean {
-    return (s.charCodeAt(0) === s.charCodeAt(1) - 1)
-};
+  const bracketMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  let openingBracketStack: string[] = [];
+
+  for (let index = 0; index < s.length; index++) {
+    if (["(", "[", "{"].includes(s[index])) {
+      openingBracketStack.push(s[index]);
+    } else if (openingBracketStack.pop() !== bracketMap[s[index]]) {
+      return false;
+    }
+  }
+
+  return !openingBracketStack.length;
+}
