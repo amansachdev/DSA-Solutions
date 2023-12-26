99ea64d91699cdd95fc246dcfcc1efd465b6dfbd
aman.s4 <aman.s@kinaracapital.com>
2023-12-26 14:24:57 +0530

    Beats 87.67%of users with TypeScript

--- 53. Maximum Subarray.ts
+++ 53. Maximum Subarray.ts
@@ -3,21 +3,22 @@ function maxSubArray(nums: number[]): number {
   let arrayOfSolutions = [];
 
   if (nums.length === 1) {
-    return nums[0];
+      return nums[0];
   }
 
   for (let index = 0; index < nums.length; index++) {
-    if (maxSum < nums[index]) {
-      maxSum = maxSum + nums[index];
-      arrayOfSolutions.push(maxSum);
-    } else if (nums[index] + maxSum < 0) {
-      maxSum = 0;
-      arrayOfSolutions.push(maxSum);
-    } else {
-      maxSum = maxSum + nums[index];
-      arrayOfSolutions.push(maxSum);
-    }
-  }
+      if (maxSum < nums[index]) {
+          maxSum = maxSum + nums[index];
+          arrayOfSolutions.push(maxSum);
+      } else if ((nums[index] + maxSum) < 0) {
+          arrayOfSolutions.push(nums[index]);
+          maxSum = 0;
 
+      } else {
+          maxSum = maxSum + nums[index]
+          arrayOfSolutions.push(maxSum);
+
+      }
+  }
   return Math.max(...arrayOfSolutions);
-}
+};
